3f3e4ae18169ba720b14d80b3118f00418da7b20
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-05 10:31:34 -0700

    new codewars added

---
+++ 8kyu/grasshopper-sayhello.js
@@ -0,0 +1,13 @@
+//Description:
+//Fix the code and get the program working again.
+//Example output: Hello, Mr.Spock
+
+//My solutions:
+function sayHello(name){
+    return `Hello, ${name}`
+}
+
+//option 2: 
+function sayHello(name){
+    return 'Hello, ' + name
+}
