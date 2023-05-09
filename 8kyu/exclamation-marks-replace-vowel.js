0405de32199ef7a8ebac028bdce5a1b7175cf186
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-09 15:05:44 -0700

    code modified

--- 8kyu/exclamation-marks-replace-vowel.js
+++ 8kyu/exclamation-marks-replace-vowel.js
@@ -1,8 +1,10 @@
 //Descriptions: 
-
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
 
 //My solutions:
 function replace(s){
     return s.replace(/[aeiou]/ig, '!')
 }
+
+const replace = s => s.replace(/[aeiou]/ig, '!')
