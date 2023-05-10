964c73696e2941ca3720d93be456f56f473550c1
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-10 11:30:08 -0700

    new codewars added

---
+++ 8kyu/get-character-from-ascii.js
@@ -0,0 +1,8 @@
+//Descriptions:
+//Write a function get_char() / getChar() which takes a number and 
+//returns the corresponding ASCII char for that value
+
+//My solutions: 
+function getChar(c){
+    return String.fromCharCode(c)
+}
