419bb53e5f68c6547b10fab97d30f076c8efcf24
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-15 20:03:07 -0700

    new codewars added

---
+++ 8kyu/is-this-my-tail.js
@@ -0,0 +1,22 @@
+//Descriptions: 
+// Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps 
+// the animals do not have the right tails. To help her, you must correct the broken 
+// function to make sure that the second argument (tail), is the same as the last 
+// letter of the first argument (body) - otherwise the tail wouldn't fit!
+// If the tail is right return true, else return false.
+// The arguments will always be non empty strings, and normal letters.
+
+//My solutions: 
+function correctTail(bod,tail){
+    if (bod[bod.length-1] === tail){
+      return true
+    }else{
+      return false
+    }
+}
+
+//options 2:
+
+function correctTail(bod,tail){
+    return bod[bod.length-1] === tail? true : false
+}
