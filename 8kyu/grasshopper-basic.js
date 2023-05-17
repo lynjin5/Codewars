89c92e2b9f68db8b1525455b1fb5220d4f7c039e
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-17 10:29:56 -0700

    new codewars added

---
+++ 8kyu/grasshopper-basic.js
@@ -0,0 +1,8 @@
+//Descriptons:
+//create a function that adds five to any number that was passed in to it 
+//and return the new value
+
+//My solutions: 
+function addFive(num) {
+    return num + 5
+}
