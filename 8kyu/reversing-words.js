f6f57b9751a368fec909a44ad071b724a01c7ca1
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-17 10:41:24 -0700

    new codewars added

---
+++ 8kyu/reversing-words.js
@@ -0,0 +1,8 @@
+//Descriptions:
+//You need to write a function that reverses the words in a given string. 
+
+//My solutions: 
+function reverse(string){
+    return string.split(' ').reverse().join(' ')
+}
+
