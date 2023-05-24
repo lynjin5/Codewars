8c18e767ec3fce5e08d7a1a5851b36ee27fcb04e
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-24 10:26:58 -0700

    new codewars added

---
+++ 8kyu/pre-fizzbuzz.js
@@ -0,0 +1,12 @@
+//Descriptions: 
+//Your inputs: a positive integer, n, greater or equal to one. n is provided, 
+//you have no control over its value. Your expected output is an array of positive integers
+//from 1 to n(inclusive). Write an algorithm that gets you from input to output. 
+
+//My solutions:
+function preFizz(n){
+    let arr = []
+    for(let i=1; i<=n; i++){
+        arr.push(i)
+    }return arr
+}
