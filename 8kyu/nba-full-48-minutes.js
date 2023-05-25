758d1a1a16521199c7606bb27102e7415637d096
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-25 15:29:49 -0700

    new codewars added

---
+++ 8kyu/nba-full-48-minutes.js
@@ -0,0 +1,33 @@
+//Descriptions:
+// An NBA game runs 48 minutes (Four 12 minute quarters). 
+// Players do not typically play the full game, subbing in and out as necessary. 
+// Your job is to extrapolate a player's points per game if they played the full 
+// 48 minutes.
+
+// Write a function that takes two arguments, ppg (points per game) and 
+// mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes 
+// rounded to the nearest tenth. Return 0 if 0.
+
+// Examples:
+
+// pointsPer48(12, 20) // 28.8
+// pointsPer48(10, 10) // 48 
+// pointsPer48(5, 17) // 14.1 
+// pointsPer48(0, 0) // 0
+
+//My solutions:
+function pointsPer48(ppg, mpg) {
+    if (mpg ==0){
+      return 0
+    }
+    return +(ppg/mpg*48).toFixed(1)
+}
+
+//Option 2:
+const pointsPer48 = (ppg,mpg) => mpg? +(ppg/mpg*48).toFixed(1) : 0
+
+//option 3:
+function pointsPer48(ppg, mpg) {
+    return +(ppg/mpg*48).toFixed(1)||0
+}
+  
