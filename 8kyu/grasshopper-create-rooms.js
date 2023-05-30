986adbed62994c6d470cf5de09f75fb7746d62f7
Lindsay Jin <lindsayjin1@gmail.com>
2023-05-29 20:51:31 -0700

    new codewars added

---
+++ 8kyu/grasshopper-create-rooms.js
@@ -0,0 +1,26 @@
+//Descriptions: 
+// You are creating an "Escape the room" game. The first step is to create a hash table 
+// called rooms that contains all of the rooms of the game. There should be at least 
+// 3 rooms inside it, each being a hash table with at least three properties 
+// (e.g. name, description, completed).
+
+//My solutions:
+var rooms = {
+    room1 :{
+      name: 'room1',
+      description: 'scary',
+      completed: 'yes'
+    },
+    room2 :{
+      name: 'room2',
+      description: 'scary',
+      completed: 'yes'
+    },
+    room3 :{
+      name: 'room3',
+      description: 'scary',
+      completed: 'yes'
+    }
+  }
+
+  //should have at least 3 rooms, each room is an object, contain 3 properties per room
