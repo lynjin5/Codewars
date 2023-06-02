b6e73b2f9c97d365ef2009175840dbe6b39178d8
Lindsay Jin <lindsayjin1@gmail.com>
2023-06-01 21:23:10 -0700

    new codewars added

---
+++ 8kyu/returning-string.js
@@ -0,0 +1,8 @@
+//Descriptions:
+// Make a function that will return a greeting statement that uses an input; 
+// your program should return, "Hello, <name> how are you doing today?".
+
+//My solutions: 
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
