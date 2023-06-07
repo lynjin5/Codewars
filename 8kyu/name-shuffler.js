52590bf85ef948bf0b81e0484a59dc1047056a14
Lindsay Jin <lindsayjin1@gmail.com>
2023-06-07 15:17:00 -0700

    new codewars added

---
+++ 8kyu/name-shuffler.js
@@ -0,0 +1,11 @@
+//Descriptions:
+//Write a function that returns a string in which firstname is swapped with last name.
+
+//Example(Input --> Output)
+
+//"john McClane" --> "McClane john"
+
+//My solutions: 
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
