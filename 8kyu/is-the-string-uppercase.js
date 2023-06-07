92a23dec633073f679e32b4578c7bd0c754edc3c
Lindsay Jin <lindsayjin1@gmail.com>
2023-06-07 15:57:05 -0700

    new codewars added

---
+++ 8kyu/is-the-string-uppercase.js
@@ -0,0 +1,17 @@
+//Descriptions:
+// Is the string uppercase?
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+//My soutions: 
+String.prototype.isUpperCase = function() {
+    return this == this.toUpperCase()
+}
